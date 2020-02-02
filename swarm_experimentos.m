5a7d53d3e92a11a9f15d2bc52df5e36b909fbee2
iri16009 <iri16009@uvg.edu.gt>
2020-02-02 01:11:23 -0600

    Experimentos para c1 y c2
    Este código genera varias corridas para c1 y c2 y guarda la distancia encontrada en una celda. Luego eso 10 veces está guardado en un Excel donde se encontró los mejores valores para estas constantes.

--- swarm_experimentos.m
+++ swarm_experimentos.m
@@ -6,24 +6,45 @@
 % 16009
 
 %% fase 1: init
+% Parámetros fijos por el usuario
 
 swarmsize = 10; % tamaño de la población (swarm)
 dimension = 2;  % Dimensión del espacio de búsqueda
-% Coeficientes de aceleración de 0 a 4. Generalmente son iguales y tienen
-% el valor de 2.
+gen = 0;        % Iteración inicial
+maxgen = 500;    % Máximo número de iteraciones
 
-c1 = 1;
-c2 = 1; 
+max_lim_x = 2;  % Límites del área de búsqueda
+min_lim_x = -2;
+max_lim_y = 2;
+min_lim_y = -2;
 
-gen = 0; % Iteración inicial
-maxgen = 70; % Máximo número de iteraciones
+porcentaje_error = 5/100;
 
-x = rand(swarmsize, dimension); % vector posicion (swarmsize filas y pos columnas)
-%x = randi(2,swarmsize, dimension); % vector posicion (swarmsize filas y pos columnas)
+% Para verificar porcentaje de error    
+distancias = zeros(swarmsize, 1); % Init del array con las distancias de cada partícula a la solución
+Xteorica = [1 1; 0 0]; % Solución (primera fila x,y)
+search_area = (max_lim_x-min_lim_x)*(max_lim_y-min_lim_y);
+distancia_max = sqrt(search_area*porcentaje_error/pi);
+
+% Init del array para graficar el globalbest vs iteraciones
+coste = zeros(1,gen);
+
+% Para graficar la superficie y el contour
+[X,Y] = meshgrid(-2:0.1:2);
+Z = 100*(Y-X.^2).^2+(ones(size(X))-X).^2;
+
+% Para graficar el círculo delimitador de soluciones
+xcirc = distancia_max * cos(0:pi/50:2*pi) + Xteorica(1,1);
+ycirc = distancia_max * sin(0:pi/50:2*pi) + Xteorica(1,2);
+
+% Creación de los vectores
+%x = rand(swarmsize, dimension); % vector posicion (swarmsize filas y pos columnas)
+x(:,1) = min_lim_x + (max_lim_x-min_lim_x).*rand(swarmsize,1);
+x(:,2) = min_lim_y + (max_lim_y-min_lim_y).*rand(swarmsize,1);
 v = rand(swarmsize, dimension); % vector aceleración
 
-%c = 2.7298; % Para garantizar convergencia
-K = 0.8;
+x_ini = x;
+v_ini = v;
 
 % valores óptimos en la primera generación
 cost = costFunction2(x);
@@ -31,91 +52,113 @@ cost = costFunction2(x);
 localbest = cost;
 localp = x;
 
-[globalbest, indx] = min(cost); % cuando necesitemos minimizar
+[globalbest, indx] = min(cost);
 globalp=x(indx,:);
-coste = zeros(1,gen);
 
-[X,Y]=meshgrid(-2:0.1:2);
-Z=100*(Y-X.^2).^2+(ones(size(X))-X).^2;
-  
-xcirc = distancia_max * cos(0:pi/50:2*pi) + 1;
-ycirc = distancia_max * sin(0:pi/50:2*pi) + 1;
-
+% Parámetros a variar
+c1 = 0:0.1:4;
+c2 = 0:0.1:4; 
+K = 1;
+funcion_w = 'constante';
 
 %% fase 2: loop
 
-while (gen < maxgen)
-    
-    gen = gen + 1;
-    
-    if funcion_w == 'lineal'
-        w = (maxgen-gen)/maxgen;
-    elseif funcion_w == 'exponencial'
-        w = exp(-1*(1-(maxgen-gen)/maxgen));
-    end
-    
-    r1 = rand(swarmsize, dimension);% Se cambia en cada iteracion?
-    r2 = rand(swarmsize, dimension);
+% PARÁMETROS
+lim_inf_c2 = 0.1;
+lim_sup_c2 = 4;
+step_c2 = 0.1;
+lim_inf_c1 = lim_inf_c2;
+lim_sup_c1 = lim_sup_c2;
+step_c1 = step_c2;
 
-    v = K.*(w.*v+c1.*r1.*(localp-x)+c2.*r2.*(ones(swarmsize, 1)*globalp-x));
-    
-    x = x+v;
-    
-    % Encontrar nada más el máximo entre -10 y 10 porque la función oscila.
-    overlimit = x(:,1) <= 2;
-    underlimit=x(:,1)>=-2;
-    x(:,1) = x(:,1).*overlimit + not(overlimit)*2;
-    x(:,1) = x(:,1).*underlimit + not(underlimit)*-2;
-    
-    overlimit = x(:,2) <= 2;
-    underlimit=x(:,2)>=-2;
-    x(:,2) = x(:,2).*overlimit + not(overlimit)*2;
-    x(:,2) = x(:,2).*underlimit + not(underlimit)*-2;
-    
-    cost = costFunction2(x);
-    
-  bestcost = cost < localbest;
-  localbest = localbest.*not(bestcost)+cost.*bestcost;
-  localp(bestcost,:) = x(bestcost,:); 
+% Creación de la celda con sus encabezados
+% El tamaño de la celda en este caso solo fue definido por el tamaño de los
+% límites de c2. Si es necesario que no sea una matriz cuadrada hay que
+% modificar el código.
 
-   [new_globalbest, indx]=min(localbest);
-    
-    if new_globalbest<globalbest
-     globalp=x(indx,:);
-     globalbest=new_globalbest;
-    end
-    
-    coste(1,gen)=globalbest;
-    subplot(1,2,1);
-    plot(coste(1:gen),'r'),grid on,xlabel('Number of generation'),ylabel('Global cost'),title('Parameter identification of nonlinear dynamic systems');
-    subplot(1,2,2);
-    scatter(x(:,1), x(:,2),20,'filled', 'k');
-    hold on
-    plot(xcirc, ycirc,'k--');
-    mytitleText = ['c_1= ',num2str(c1),' c_2 = ', num2str(c2),' K = ', num2str(K)];
-    title(mytitleText,'Interpreter','tex' );
-    contour(X,Y,Z,50)
-    scatter(1, 1,80, 'r','x','LineWidth',1.5);
-    hold off
-    drawnow;
-    pause(0.1);
+celda = cell((lim_sup_c2-lim_inf_c2)/step_c2+2);
+contc1 = 2;
+for c2 = lim_inf_c2:step_c2:lim_sup_c2
+    celda{1,contc1} = num2str(c2);
+    celda{contc1,1} = num2str(c2);
+    contc1 = contc1 + 1;
 end
 
-distancias = zeros(swarmsize, 1);
-Xteorica = [1 1; 0 0];
-
-search_area = 16;
-porcentaje_error = 1/100;
-
-distancia_max = sqrt(search_area*porcentaje_error/pi);
-
-cumplen_porcentaje_error = 0;
-
-for p = 1:swarmsize
-        Xteorica(2,:) = x(p,:);
-        distancias(p) = pdist(Xteorica);
-        if (distancias(p) <= distancia_max)
-            cumplen_porcentaje_error = cumplen_porcentaje_error + 1;
+contc1 = 2;
+contc2 = 2;
+for c1= lim_inf_c1:step_c1:lim_sup_c1
+    for c2 = lim_inf_c2:step_c2:lim_sup_c2
+        
+        while (gen < maxgen)
+            
+            gen = gen + 1;
+            
+            if strcmp(funcion_w,'lineal')
+                w = (maxgen-gen)/maxgen;
+            elseif strcmp(funcion_w,'exp')
+                w = exp(-1*(1-(maxgen-gen)/maxgen));
+            elseif strcmp(funcion_w,'constante')
+                w = 1;
+            end
+            
+            r1 = rand(swarmsize, dimension);% Se cambia en cada iteracion?
+            r2 = rand(swarmsize, dimension);
+            
+            v = K.*(w.*v+c1.*r1.*(localp-x)+c2.*r2.*(ones(swarmsize, 1)*globalp-x));
+            
+            x = x+v;
+            
+            % Encontrar nada más el máximo entre -10 y 10 porque la función oscila.
+            overlimitx = x(:,1) <= max_lim_x;
+            underlimitx = x(:,1) >= min_lim_x;
+            x(:,1) = x(:,1).*overlimitx + not(overlimitx)*max_lim_x;
+            x(:,1) = x(:,1).*underlimitx + not(underlimitx)*min_lim_x;
+            
+            overlimity = x(:,2) <= max_lim_y;
+            underlimity = x(:,2)>=min_lim_y;
+            x(:,2) = x(:,2).*overlimity + not(overlimity)*max_lim_y;
+            x(:,2) = x(:,2).*underlimity + not(underlimity)*min_lim_y;
+            
+            cost = costFunction2(x);
+            
+            bestcost = cost < localbest;
+            localbest = localbest.*not(bestcost)+cost.*bestcost;
+            localp(bestcost,:) = x(bestcost,:);
+            
+            [new_globalbest, indx] = min(localbest);
+            
+            if new_globalbest < globalbest
+                globalp = x(indx,:);
+                globalbest = new_globalbest;
+            end
+            
         end
+        
+        % Verificación de porcentaje de error del algoritmo
+        cumplen_porcentaje_error = 0;
+        for p = 1:swarmsize
+            Xteorica(2,:) = x(p,:);
+            distancias(p) = pdist(Xteorica);
+            if (distancias(p) <= distancia_max)
+                cumplen_porcentaje_error = cumplen_porcentaje_error + 1;
+            end
+        end
+        gen = 0;
+        x = x_ini;
+        v = v_ini;
+        cost = costFunction2(x);
+        localbest = cost;
+        localp = x;
+        [globalbest, indx] = min(cost);
+        globalp=x(indx,:);
+
+        celda{contc2,contc1} = cumplen_porcentaje_error;
+        contc2 = contc2 +1;
+    end
+    contc2 = 2;
+    contc1 = contc1 + 1;
 end
+
+
+
 
